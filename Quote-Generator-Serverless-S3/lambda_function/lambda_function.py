58d5ea08a75850fd47e16489ed1aec4ec8aaf326
Trevor Oliver <120150007+Trevorkutto@users.noreply.github.com>
2025-06-06 15:35:50 -0400

    Update lambda_function.py

--- Quote-Generator-Serverless-S3/lambda_function/lambda_function.py
+++ Quote-Generator-Serverless-S3/lambda_function/lambda_function.py
@@ -6,6 +6,9 @@ quotes = [
     {"text": "Innovation distinguishes between a leader and a follower.", "author": "Steve Jobs"},
     {"text": "Stay hungry, stay foolish.", "author": "Steve Wozniak"},
     {"text": "The future belongs to those who believe in the beauty of their dreams.", "author": "Eleanor Roosevelt"}
+    { text: "It is during our darkest moments that we must focus to see the light.", author: "Aristotle" },
+    { text: "Success is not final, failure is not fatal: it is the courage to continue that counts.", author: "Winston Churchill" },
+    { text: "The only impossible journey is the one you never begin.", author: "Tony Robbins" },
 ]
 
 def lambda_handler(event, context):
